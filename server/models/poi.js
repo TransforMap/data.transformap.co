1d260b252d5387235c23b1d421226bc9a4300097
Michael Maier <michael.maier@student.tugraz.at>
2016-02-12 19:13:30 +0100

    wrote more tests for the create model

--- server/models/poi.js
+++ server/models/poi.js
@@ -1,12 +1,27 @@
-const CONFIG = require('config')
-const __ = CONFIG.universalPath
-const _ = __.require('lib', 'utils')
+// const CONFIG = require('config')
+// const __ = CONFIG.universalPath
+// const _ = __.require('lib', 'utils')
 
 module.exports = {
   create: function (doc) {
+    if (doc.type !== 'poi') {
+      throw new Error('not of type POI')
+    }
     var name = doc.name
-    if (name == null || name === '' ) {
+    if (name == null || name === '') {
       throw new Error('missing name')
+    }
+    if (doc.lat == null || doc.lon == null) {
+      throw new Error('missing coordinate/s')
+    }
+    if (typeof doc.lat !== 'number' || typeof doc.lon !== 'number') {
+      throw new Error('coordinates not of type number')
+    }
+    if (doc.lat < -90 || doc.lat > 90) {
+      throw new Error('coordinate lat out of range')
+    }
+    if (doc.lon < -180 || doc.lon > 360) {
+      throw new Error('coordinate lon out of range')
     }
     return doc
   }
--- test/poi-model.js
+++ test/poi-model.js
@@ -12,22 +12,50 @@ describe('poi model', function () {
     })
     it('should return an object if the doc is valid', function (done) {
       var doc = {
-        name: 'Jon'
+        name: 'Jon',
+	type: 'poi',
+	lat: 1,
+	lon: 1
       }
       var create = function () { return Poi.create(doc) }
       create.should.not.throw()
       create().should.be.an.Object()
       done()
     })
+    it('should throw if not of type poi', function (done) {
+      var doc = { name: "K", lat: 1, lon: 1 }
+      var create = function () { Poi.create(doc) }
+      create.should.throw()
+      done()
+    })
+    it('should throw if the coordinates are not provided', function (done) {
+      var doc = { name: "K", lat: null, lon: null, type: "poi" }
+      var create = function () { Poi.create(doc) }
+      create.should.throw()
+      done()
+    })
+    it('should throw if the coordinates are of type number', function (done) {
+      var doc = { name: "K", lat: "1", lon: "1" , type: "poi"}
+      var create = function () { Poi.create(doc) }
+      create.should.throw()
+      done()
+    })
+    it('should throw if the coordinates are out of bounds', function (done) {
+      var doc = { name: "K", lat: -200, lon: 2000 , type: "poi"}
+      var create = function () { Poi.create(doc) }
+      create.should.throw()
+      done()
+    })
     it('should throw if the doc has no name', function (done) {
-      var doc = {}
+      var doc = {lat: "1", lon: "1" , type: "poi"}
       var create = function () { Poi.create(doc) }
       create.should.throw()
       done()
     })
     it('should throw if the name is empty', function (done) {
       var doc = {
-        name: ''
+        name: '',
+        lat: "1", lon: "1" , type: "poi"
       }
       var create = function () { Poi.create(doc) }
       create.should.throw()
