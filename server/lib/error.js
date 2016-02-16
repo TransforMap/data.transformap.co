e9adbd52462c27e40e51b9a87be5a697e7e1c350
maxlath <k@maxlath.eu>
2016-02-16 14:56:08 +0100

    re-qualifying Couchdb errors at error handler for instance, when Couchdb returns a 404 for a missing view or design document it should return a server 500 error

--- server/lib/error.js
+++ server/lib/error.js
@@ -12,17 +12,37 @@ const newErr = function (message, status, data) {
   return formatError(new Error(message), status, data)
 }
 
-handler = function (res, err) {
-  const statusCode = err.statusCode
+const handler = function (res, err) {
+  var statusCode = err.statusCode
   const message = err.message
+
   if (statusCode < 500) {
-    _.warn(err.data, message)
+    if (isCouchError(err)) {
+      if (isCouchHardError(err)) {
+        _.error(err.request, `couchdb Error: ${err.message}`)
+        // re-qualify the error as a 500 as it is the server's fault
+        statusCode = 500
+      } else {
+        _.warn(err.request, message)
+      }
+    } else {
+      _.warn(err.data, message)
+    }
     res.status(statusCode).json({status: message})
   } else {
-    _.error(err, 'handler')
+    _.error(err, `request Error: ${err.message}`)
     res.status(statusCode || 500).json({status: message})
   }
 }
+
+const isCouchError = function (err) {
+  return err.scope === 'couch'
+}
+// couchdb errors reason that are the server's fault
+const isCouchHardError = function (err) {
+  return !_.includes(softCouchErrors, err.reason)
+}
+const softCouchErrors = ['missing']
 
 module.exports = {
   new: newErr,
