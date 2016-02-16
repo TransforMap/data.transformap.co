373cd663db7ca98bbe17f4dc1265f90770a27e93
maxlath <k@maxlath.eu>
2016-02-16 16:10:26 +0100

    cleaning

--- server/lib/db/db.js
+++ server/lib/db/db.js
@@ -3,9 +3,6 @@ const buildDbApi = require('./build_db_api')
 
 module.exports = function (dbName, designName) {
   const db = nano.use(dbName)
-  // /!\ we are not returning the ensureDbExistance promise
-  // so the database creation might happen after we return
-  // ensureDbExistance(dbName, db)
   // generate an API tailored to our needs
   // rather than the raw nano API
   return buildDbApi(db, designName)
