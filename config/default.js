77c9b5e70a3bed90b06402a4bd646a138186e63a
maxlath <k@maxlath.eu>
2016-02-18 16:43:52 +0100

    adding some integration tests

--- config/default.js
+++ config/default.js
@@ -1,8 +1,12 @@
 module.exports = {
   universalPath: require('./universal_path'),
   server: {
+    protocol: 'http',
     host: '0.0.0.0',
-    port: 8726
+    port: 8726,
+    url: function () {
+      return `${this.protocol}://${this.host}:${this.port}`
+    }
   },
   db: {
     protocol: 'http',
