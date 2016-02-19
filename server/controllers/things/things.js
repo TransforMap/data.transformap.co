3ee5abdaa184d15b8f55e4b1a9f0951bc788f09e
maxlath <k@maxlath.eu>
2016-02-19 03:34:57 +0100

    inducing the contextsList from available models

--- server/controllers/things/things.js
+++ server/controllers/things/things.js
@@ -1,8 +1,15 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const _ = __.require('lib', 'utils')
-const contextsList = [ 'place' ]
 const buildController = require('./build_controller')
+
+// generating the list of contexts names from the files
+// available in the models/contexts folder
+const extractName = (filename) => filename.split('.')[0]
+const folder = __.path('controllers', 'things/models/contexts')
+const contextsList = require('fs').readdirSync(folder).map(extractName)
+
+_.log(contextsList, 'contexts')
 
 module.exports = {
   generateRoutes: function () {
@@ -17,11 +24,13 @@ module.exports = {
 const contextRoutes = function (contextName) {
   const controller = buildController(contextName)
   const routes = {}
+
   routes[contextName] = {
     post: controller.post
   }
   routes[`${contextName}/:id`] = {
     get: controller.get
   }
+
   return routes
 }
