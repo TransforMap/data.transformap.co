dc20e81e5725cbd86bdb267cc631173da6de8a1e
Vincent <vincent.jumeaux@gmail.com>
2017-09-13 18:04:30 +0200

    setup findOrCreate user
    - fix users designdoc
    - extract unecessary build_lib to user model

--- server/controllers/users/routes.js
+++ server/controllers/users/routes.js
@@ -1,7 +1,7 @@
 const CONFIG = require('config')
 const __ = CONFIG.universalPath
 const _ = __.require('lib', 'utils')
-const lib = require('./build_lib')
+const User = require('./lib/users')
 const error_ = __.require('lib', 'error')
 
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
       "users": {
         // TODO implement ensureAuthenticated from branch 425 oauth
         post: function (req, res) {
-          lib.create(req.body)
+          User.create(req.body)
           .then(res.json.bind(res))
           .catch(function (err) {
             error_.bundle(res, err, 400, req.body)
@@ -20,7 +20,7 @@ module.exports = {
       "users/:id": {
         get: function (req, res) {
           const id = req.params.id
-          lib.byId(id)
+          User.byId(id)
           .then(res.json.bind(res))
           .catch(error_.Handler(res))
         }
--- test/user-model.js
+++ test/user-model.js
@@ -7,6 +7,7 @@ const it = global.it // for lint
 const describe = global.describe // for lint
 const User = __.require('controllers', 'users/lib/users')
 const userNewDoc = require('./fixtures/user-new-to-create-for-api')
+const data = _.cloneDeep(userNewDoc)
 
 describe('User model', function () {
   describe('validateData', function () {
@@ -45,6 +46,31 @@ describe('User model', function () {
         err.message.should.equal('missing name')
       }
       done()
+    })
+  })
+  describe('findOrCreateUser', function () {
+    username = userNewDoc.contact.name
+
+    it('should be a function that returns an object', function (done) {
+      User.findOrCreateUser.should.be.a.Function()
+      User.findOrCreateUser(userNewDoc).should.be.an.Object()
+      done()
+    })
+    it('should return an object with the passed type', function (done) {
+      User.findOrCreateUser(userNewDoc)
+      .then(function (result) {
+        result.type.should.equal("User")
+      })
+      .then(done, done)
+    })
+    it('should return a doc._id', function (done) {
+      userNewDoc.contact.name = "UnknownName"
+      User.findOrCreateUser(userNewDoc)
+      .then(function (result) {
+        const uuid = result.id || result._id
+        _.isUuid(uuid).should.equal(true)
+      })
+      .then(done, done)
     })
   })
 })
