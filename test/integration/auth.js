3a59a4e78c974cc0f055b96b5a3bbcbc6ae54f6a
Vincent <vincent.jumeaux@gmail.com>
2017-09-19 10:48:00 +0200

    better integration test cover

--- test/integration/auth.js
+++ test/integration/auth.js
@@ -1,34 +1,25 @@
 const CONFIG = require('config')
-const __ = CONFIG.universalPath
-const _ = __.require('lib', 'utils')
-
-const it = global.it // for lint
-const describe = global.describe // for lint
 require('should')
 
 const breq = require('bluereq')
-const get = (url) => breq.get(url).then(_.property('body'))
 const endpoint = CONFIG.server.url() + '/'
 
-const passport = require('passport'),
-  OAuth2Strategy = require('passport-oauth').OAuth2Strategy
-
-
 describe('/secretPage', function () {
-  it('should redirect to provider authentication page', function (done) {
+  it('should redirect to provider authentication page in the browser - 406 in this testing env', function (done) {
     breq.get(`${endpoint}secretPage`)
     .then(function (body) {
-      body.req.path.should.equal("/users/sign_in")
+      body.req.path.should.equal('/users/sign_in')
+      body.statusCode.should.equal(406)
       done()
     })
   })
 })
-describe('/auth/logout', function () {
-  it('it should end the session and show a public page', function (done) {
-    breq.get(`${endpoint}auth/logout`)
-    .then(function(body) {
-      body.req.path.should.equal("/auth/logout")
-      done();
-    });
-  });
-});
+describe('/logout', function () {
+  it('should end the session and show a public page', function (done) {
+    breq.get(`${endpoint}logout`)
+    .then(function (body) {
+      body.statusCode.should.equal(200)
+      done()
+    })
+  })
+})
