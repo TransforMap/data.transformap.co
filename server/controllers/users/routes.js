a1aab6c2d29d79dc32eaaf87dbb4af931a2d95bd
Vincent <vincent.jumeaux@gmail.com>
2017-10-08 15:03:21 +0200

    open an whoAmI endpoint which bind to response the user store if authenticated

--- server/controllers/users/routes.js
+++ server/controllers/users/routes.js
@@ -3,27 +3,21 @@ const __ = CONFIG.universalPath
 const _ = __.require('lib', 'utils')
 const User = require('./lib/users')
 const error_ = __.require('lib', 'error')
+const _passport = require('./../../middlewares/passport')
 
 module.exports = {
   generateRoutes: function () {
     return {
-      "users": {
-        // TODO implement ensureAuthenticated from branch 425 oauth
-        post: function (req, res) {
-          User.create(req.body)
-          .then(res.json.bind(res))
-          .catch(function (err) {
-            error_.bundle(res, err, 400, req.body)
-          })
-        }
-      },
-      "users/:id": {
-        get: function (req, res) {
-          const id = req.params.id
-          User.byId(id)
-          .then(res.json.bind(res))
-          .catch(error_.Handler(res))
-        }
+      "user": {
+        get: [
+          _passport.ensureAuthenticated,
+          function (req, res) {
+            const whoAmI = req.session.passport.user
+            User.byId(whoAmI)
+            .then(res.json.bind(res))
+            .catch(error_.Handler(res))
+          }
+        ]
       }
     }
   }
