d753c9b2081c326bb1bfabde772b55112e497857
Vincent <vincent.jumeaux@gmail.com>
2017-10-30 21:03:12 +0100

    update cors to allow passport cross domain cookie
    - extend cookie life to 30days

--- server/middlewares.js
+++ server/middlewares.js
@@ -3,11 +3,17 @@ const CONFIG = require('config')
 const passport = require('./middlewares/passport')
 
 const cors = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
+  if ('OPTIONS' == req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
 }
+
 
 module.exports = {
   common: [
@@ -20,7 +26,8 @@ module.exports = {
       cookie: {
         path    : '/',
         httpOnly: true,
-        maxAge  : 24*60*60*1000
+        maxAge  : 30*24*60*60*1000,
+        domain  : 'allmende.io'
       },
       secret: CONFIG.get('auth.passportSessionSecret'),
       resave: true,
