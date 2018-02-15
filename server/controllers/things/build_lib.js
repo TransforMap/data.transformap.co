b9f047fb9ee9524d8b1d24665a9c870abff8dd13
almereyda <almereyda@allmende.io>
2018-02-15 10:39:11 +0100

    happy linters make us happy

--- server/controllers/things/build_lib.js
+++ server/controllers/things/build_lib.js
@@ -15,36 +15,35 @@ module.exports = function (typeName, model) {
       return db.viewByKey('byId', [typeName, id])
       .then(_.Log(`${typeName} byId`))
     },
-    filter: function(filter_string) {
+    filter: function (filter_string) {
       const uriBuilder = function (item, hostname, item_type) {
         return `${hostname}/${item_type}/${item.journal}`
       }
 
       const convertArrayToTypeFeatureCollection = function (data_array) {
-        return libUtils.convertArrayToFeatureCollection(data_array,typeName,`/${filter_string}`, uriBuilder)
+        return libUtils.convertArrayToFeatureCollection(data_array, typeName, `/${filter_string}`, uriBuilder)
       }
 
-      if(filter_string == '') {
+      if (filter_string === '') {
         return db.viewByKeyRange('currentVersionById', [typeName, '0'], [typeName, 'z'])
         .then(convertArrayToTypeFeatureCollection)
         .then(_.Log(`${typeName} filter all`))
       } else {
-        const key = filter_string.replace(/=.*$/,''),
-              value = filter_string.split('=')[1]
+        const key = filter_string.replace(/=.*$/, '')
+        const value = filter_string.split('=')[1]
 
         var startkey, endkey
-        if(value) {
+        if (value) {
           startkey = [typeName, key, value]
           endkey = startkey
         } else {
           startkey = [typeName, key, ' ']
-          endkey =  [typeName, key, '\u9999']
+          endkey = [typeName, key, '\u9999']
         }
         return db.viewByKeyRange('byAttribute', startkey, endkey)
         .then(convertArrayToTypeFeatureCollection)
         .then(_.Log(`${typeName} filter byAttribute "${key}"="${value}"`))
       }
-
     },
     create: function (data) {
       // first make sure we have valid data
@@ -87,7 +86,7 @@ module.exports = function (typeName, model) {
       }
 
       const convertArrayToVersionTypeFeatureCollection = function (resultArray) {
-        return libUtils.convertArrayToFeatureCollection(resultArray, typeName, `/${id}/versions`, uriBuilder )
+        return libUtils.convertArrayToFeatureCollection(resultArray, typeName, `/${id}/versions`, uriBuilder)
       }
 
       const startKey = [typeName, id]
@@ -105,7 +104,7 @@ module.exports = function (typeName, model) {
 
       return db.viewByKeyRange('versionsById', startKey, endKey)
       .then(_.Log('viewByKeyRangeResult'))
-      .then(function (result) { return result[0] } )
+      .then(function (result) { return result[0] })
       .then(_.Log('extractedItem'))
       .then(libUtils.reject404)
       .then(Version.parseCurrentVersion)
